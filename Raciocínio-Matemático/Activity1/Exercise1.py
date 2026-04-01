c5c6d862bd97b2b4da32672ec6c1119b75265468
Felipe Genevro <felipegenevro6@gmail.com>
2026-04-01 12:32:59 -0300

    lista de exercicios de conjuntos e números complexos em python

--- Raciocínio-Matemático/Activity1/Exercise1.py
+++ Raciocínio-Matemático/Activity1/Exercise1.py
@@ -1,28 +1,48 @@
+#Lista de exercícios de conjuntos e números complexos passados pela professora Roveredo
+
+from math import sqrt, pi
+
+print("\n>>>PÁGINA 2<<<")
+
+#=================================
+#Atividade 1
+#=================================
+
 S = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 A = {3, 6, 5, 8, 1, 7}
 B = {2, 3, 4, 5, 6, 7}
 C = {1, 8, 9, 7}
 
-print(f'S = {S}')
+print("\n✅ | Atividade 1")
+print(f"\nA) A' = {S-A}")
+print(f"B) B' = {S-B}")
+print(f"C) C' = {S-C}")
+print(f"D) AuB = {A.union((S-B))}")
+print(f"E) AnC = {A.intersection(C)}")
+print(f"F) (AnC)' = {S-(A.intersection(C))}")
+print(f"G) (A-B)uC = {(A-B).union(C)}")
+print(f"H) (A-B)'nC' = {(S-(A-B)).intersection(S-C)}")
+print(f"I) A'nC' = {(S-A).intersection(S-C)}")
 
-A_ = S-A
-B_ = S-B
-C_ = S-C
 
-print(f'A_ = {A_}')
-print(f'B_ = {B_}')
-print(f'C_ = {C_}')
+#=================================
+#Atividade 2
+#=================================
 
-AuB = A.union(B)
-AnB = A.intersection(B)
+A = {1, 3, 5, pi, 7, 8, (9, 10)}
+B = {3, 5, pi}
+C = (pi, (9))
 
-print(f'AuB = {AuB}')
-print(f'AnB = {AnB}')
+D = (9, 10)
 
-A_BnC = (A-B).union(C)
-print(f'A_BnC = {A_BnC}')
+print("\n✅ | Atividade 2")
 
-if 10 in S or 12 in S:
-    print("true")
+if 9 in A:
+    print("\nA) True")
 else:
-    print("false")
+    print("\nA) False")
+
+if {9, 10} <= A:
+    print("B) True")
+else:
+    print("B) False")
---
+++ Raciocínio-Matemático/Activity1/Exercise2.py
@@ -0,0 +1,8 @@
+z1 = complex(3, 4)
+z2 = complex(1, 2)
+z3 = complex(7, 9)
+
+print(f"A) {z1 + z2}")
+print(f"B) {z2 - z3}")
+print(f"C) {z1 * z3}")
+print(f"D) {z2 / z3:.2f}")
