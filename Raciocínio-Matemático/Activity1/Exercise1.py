175e464767089b6680dce434577db8b40bae6f1d
Felipe Genevro <felipegenevro6@gmail.com>
2026-04-01 20:56:41 -0300

    lista de exercicios de conjuntos e números complexos em python

--- Raciocínio-Matemático/Activity1/Exercise1.py
+++ Raciocínio-Matemático/Activity1/Exercise1.py
@@ -2,6 +2,10 @@
 
 from math import sqrt, pi
 
+#=================================
+#PÁGINA 1
+#=================================
+
 print("\n>>>PÁGINA 2<<<")
 
 #=================================
@@ -33,8 +37,6 @@ A = {1, 3, 5, pi, 7, 8, (9, 10)}
 B = {3, 5, pi}
 C = (pi, (9))
 
-D = (9, 10)
-
 print("\n✅ | Atividade 2")
 
 if 9 in A:
@@ -46,3 +48,73 @@ if {9, 10} <= A:
     print("B) True")
 else:
     print("B) False")
+
+if C in A:
+    print("C) True")
+else:
+    print("C) False")
+
+if B < A:
+    print("D) True")
+else:
+    print("D) False")
+
+#=================================
+#Atividade 3
+#=================================
+
+z1 = complex(3, 4)
+z2 = complex(1, 2)
+z3 = complex(7, 9)
+
+print("\n✅ | Atividade 3")
+
+print(f"\nA) Z1 + Z2 = {z1 + z2}")
+print(f"B) Z2 - Z3 = {z1 - z2}")
+print(f"C) Z1 * Z3 = {z1 * z3}")
+print(f"D) Z2 / Z3 ≈ {z2 / z3:.2f}")
+
+#=================================
+#PÁGINA 2
+#=================================
+
+print("\n>>>PÁGINA 1<<<")
+
+#=================================
+#Atividade 1
+#=================================
+
+S = {2, 5, 17, 27}
+
+print("\n✅ | Atividade 1")
+
+if 5 in S:
+    print("\nA) True")
+else:
+    print("\nA) False")
+
+if 2 + 5 in S:
+    print("B) True")
+else:
+    print("B) False")
+
+if () in S:
+    print("C) True")
+else:
+    print("C) False")
+
+if S in S:
+    print("D) True")
+else:
+    print("D) False")
+
+#=================================
+#Atividade 2
+#=================================
+
+R = {1, 3, pi, 4, 1, 9, 10}
+T = {1, 3, pi}
+S = {(1), 3, 9, 10}
+U = {(1, 3, pi), 1}
+
+print("\n✅ | Atividade 2")
